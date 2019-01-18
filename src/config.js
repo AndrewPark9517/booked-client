9f971bcc07cfe9ee46a5d9ca0bfddb71eb48858a
Andrew Park <13parksangjoon@gmail.com>
2019-01-17 18:23:11 -0800

    config correction

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'https://booked-inventory-app-server.herokuapp.com/';
+    process.env.REACT_APP_API_BASE_URL || 'https://booked-inventory-app-server.herokuapp.com/api';
 
     //'http://localhost:8080/api'
